cb6bfdfcfa73a105e5c00e1a3bcf9830cf264fe6
hasibur14 <hasib141415@gmail.com>
2024-05-31 23:43:35 +0600

    create hostel management system

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer";
+import Navbar from "../pages/Shared/Navbar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+
+            <div>
+                <Outlet></Outlet>
+            </div>
+
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
