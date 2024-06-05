36c22a0b25abe118411d7dff015d3a5cfb9b8b4f
hasibur14 <hasib141415@gmail.com>
2024-06-06 00:48:03 +0600

    addmeal updated

--- src/pages/Dashboard/Admin/AddMeals/AddMeals.jsx
+++ src/pages/Dashboard/Admin/AddMeals/AddMeals.jsx
@@ -1,31 +1,72 @@
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import bannerImg from '../../../../assets/inputBgimg.jpg';
+import useAuth from "../../../../hooks/useAuth";
+import useAxiosPublic from '../../../../hooks/useAxiosPublic';
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+
+const image_hosting_key = import.meta.env.VITE_IMGBB_API_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddMeals = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const axiosPublic = useAxiosPublic()
 
-    const onSubmit = (data) => {
-        console.log(data); // You can handle form submission here
+    const onSubmit = async (data) => {
+        console.log(data)
+        // image upload to imgbb and then get an url
+        const imageFile = { image: data.image[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        });
+        if (res.data.success) {
+            // now send the menu item data to the server with the image url
+            const mealItem = {
+                title: data.title,
+                category: data.category,
+                price: parseFloat(data.price),
+                ingredients: data.ingredients.split(','),
+                image: res.data.data.display_url,
+                description: data.description,
+                rating: parseFloat(data.rating),
+                postTime: new Date(data.postTime).toISOString(),
+                reviews: data.reviews.split(','),
+                admin: user?.displayName,
+                adminEmail: user?.email,
+            };
+            console.log(mealItem)
+            // 
+            const mealRes = await axiosSecure.post('/meal', mealItem);
+            console.log(mealRes.data)
+            if (mealRes.data.insertedId) {
+                // show success popup
+                reset();
+                toast.success('Meal added successfully')
+            }
+        }
+        console.log('with image url', res.data);
     };
 
     return (
         <div
-            className="lg:w-[1520px] place-items-center bg-cover bg-no-repeat  rounded-md py-6 bg-blend-overlay"
-            style={{
-                backgroundImage: `url(${bannerImg})`,
-            }}
+            className="lg:w-[1520px] place-items-center bg-cover bg-no-repeat rounded-md py-6 bg-blend-overlay"
+            style={{ backgroundImage: `url(${bannerImg})` }}
         >
             <div className="max-w-4xl mx-auto p-4">
                 <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 lg:grid-cols-2">
-                    <div className="form-control ">
+                    <div className="form-control">
                         <label className="text-white p-1">Title:</label>
                         <input
                             type="text"
-                            {...register("name", { required: true })}
-                            placeholder="Enter your name here"
+                            {...register("title", { required: true })}
+                            placeholder="Enter title here"
                             className="w-full px-3 py-2 all-input-style transition-all duration-300"
                         />
-                        {errors.name && <span className="text-red-500">Title is required</span>}
+                        {errors.title && <span className="text-red-500">Title is required</span>}
                     </div>
 
                     <div className="form-control">
@@ -46,9 +87,9 @@ const AddMeals = () => {
                     <div className="form-control">
                         <label className="text-white p-1">Image URL:</label>
                         <input
-                            {...register("image", { required: true })}
-                            placeholder="Enter image URL here"
-                            className="w-full px-3 py-2 all-input-style transition-all duration-300"
+                            type="file"
+                            {...register('image', { required: true })}
+                            className="file-input w-full px-3 py-2 all-input-style transition-all duration-300"
                         />
                         {errors.image && <span className="text-red-500">Image is required</span>}
                     </div>
@@ -68,7 +109,7 @@ const AddMeals = () => {
                         <label className="text-white p-1">Ingredients:</label>
                         <textarea
                             {...register("ingredients", { required: true })}
-                            placeholder="Enter ingredients here"
+                            placeholder="Enter ingredients (comma-separated) here"
                             className="w-full px-3 py-2 all-input-style transition-all duration-300"
                         />
                         {errors.ingredients && <span className="text-red-500">Ingredients are required</span>}
@@ -83,6 +124,7 @@ const AddMeals = () => {
                         />
                         {errors.description && <span className="text-red-500">Description is required</span>}
                     </div>
+
                     <div className="form-control">
                         <label className="text-white p-1">Rating:</label>
                         <input
@@ -99,14 +141,13 @@ const AddMeals = () => {
                         <input
                             type="datetime-local"
                             {...register("postTime", { required: true })}
-                            placeholder="Enter post time here"
                             className="w-full px-3 py-2 all-input-style transition-all duration-300"
                         />
                         {errors.postTime && <span className="text-red-500">This field is required</span>}
                     </div>
 
                     <div className="form-control">
-                        <label className="text-white p-1"> Likes:</label>
+                        <label className="text-white p-1">Likes:</label>
                         <input
                             type="number"
                             {...register("likes", { required: true })}
@@ -119,11 +160,9 @@ const AddMeals = () => {
                     <div className="form-control">
                         <label className="text-white p-1">Reviews:</label>
                         <input
-                            type="number"
                             {...register("reviews", { required: true })}
-                            placeholder="Enter reviews here"
-                            className="w-full px-3 py-2 all-input-style transition-all duration-300"
-                        />
+                            placeholder="Enter reviews (comma-separated) here"
+                            className="w-full px-3 py-2 all-input-style transition-all duration-300" />
                         {errors.reviews && <span className="text-red-500">Reviews are required</span>}
                     </div>
 
